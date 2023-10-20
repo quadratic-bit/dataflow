3d1f11e461345558f8a34f3f3a746b34c5eab13c
Artem <fynjyxbrjd@gmail.com>
2023-10-20 07:02:48 +0300

    feat: populate buttons in pagination section

--- src/dom/pagination.ts
+++ src/dom/pagination.ts
@@ -1,11 +1,38 @@
 export class PagintaionDOM {
     private _container: HTMLDivElement
+    private _wrapper: HTMLDivElement
+    private _buttonsAmount: number
 
     constructor(container: HTMLDivElement) {
         this._container = container
-        const sth: HTMLSpanElement = document.createElement("span")
-        sth.textContent = "2"
-        this._container.appendChild(sth)
+        const wrapper: HTMLDivElement = document.createElement("div")
+        this._container.appendChild(wrapper)
+        this._wrapper = wrapper
+        this._buttonsAmount = 0
+        this.repopulateButtons(1)
+    }
+
+    repopulateButtons(amount: number) {
+        const previousAmount = this._buttonsAmount
+        this._buttonsAmount = amount
+        if (previousAmount == amount) {
+            return
+        } else if (previousAmount < amount) {
+            let newButtons: HTMLButtonElement[] = Array(amount - previousAmount)
+            for (let i = previousAmount + 1; i <= amount; ++i) {
+                let button = document.createElement("button")
+                button.textContent = i + ""
+                newButtons[i - previousAmount - 1] = button
+            }
+            this._wrapper.append(...newButtons)
+            return
+        }
+        // previousAmount > amount
+        for (let i = 0; i < previousAmount - amount; ++i) {
+            const lastChild = this._wrapper.lastChild
+            if (lastChild == null) break;
+            this._wrapper.removeChild(lastChild)
+        }
     }
 
     changePageNum(pagesNum: number) {
