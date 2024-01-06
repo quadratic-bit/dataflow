dbc43bdeea2c1abef692d8cb7c14289ccdd695c6
April <fynjyxbrjd@gmail.com>
2024-01-06 18:37:19 +0700

    docs: remove introduction page

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home(): JSX.Element {
                     <div className={styles.buttons}>
                         <Link
                             className="button button--secondary button--lg"
-                            to="/docs/tutorial/intro/">
+                            to="/docs/tutorial/getting-started/installation/">
                             Let's get started
                         </Link>
                     </div>
