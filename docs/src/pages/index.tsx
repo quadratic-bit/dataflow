fe9f619edb91c8ef8dcfa69c2a72e8605247dd8f
April <fynjyxbrjd@gmail.com>
2024-03-22 19:06:39 +0700

    docs: remove landing page, skip to docs/intro

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,30 +1,7 @@
-import clsx from "clsx";
-import Heading from "@theme/Heading";
-import Link from "@docusaurus/Link";
-import Layout from "@theme/Layout";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-
-import styles from "./index.module.css";
+import { Redirect } from "@docusaurus/router";
 
 export default function Home(): JSX.Element {
-    const {siteConfig} = useDocusaurusContext();
     return (
-        <Layout>
-            <header className={clsx("hero hero--primary", styles.heroBanner)}>
-                <div className="container">
-                    <Heading as="h1" className="hero__title">
-                        {siteConfig.title}
-                    </Heading>
-                    <p className="hero__subtitle">{siteConfig.tagline}</p>
-                    <div className={styles.buttons}>
-                        <Link
-                            className="button button--secondary button--lg"
-                            to="/docs/tutorial/getting-started/installation/">
-                            Let's get started
-                        </Link>
-                    </div>
-                </div>
-            </header>
-        </Layout>
+        <Redirect to="/dataflow/docs/tutorial/getting-started/installation/" />
     );
 }
